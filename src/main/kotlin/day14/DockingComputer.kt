bde472b0ef95bb17fe57ee1a6572506899dcb554
Craig Warren <craig@craigwarren.co.uk>
2020-12-14 09:47:07 +0000

    Adding solution for day 14 puzzle 1

---
+++ src/main/kotlin/day14/DockingComputer.kt
@@ -0,0 +1,49 @@
+package day14
+
+import java.io.File
+
+@ExperimentalStdlibApi
+class DockingComputer(fileName: String) {
+    val instructions = mutableListOf<Pair<String, String>>()
+    val memory = mutableMapOf<Int, Long>()
+    var currentMask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    val memoryRegex = """^mem\[(\d+)\]$""".toRegex()
+
+    init {
+        File(fileName).forEachLine {
+            val parts =  it.split(" = ")
+            instructions.add(Pair(parts[0], parts[1]))
+        }
+    }
+
+    internal fun applyMask(value: Long, mask:String): Long {
+        var chars = CharArray(36) {'0'}
+        var binaryValue = value.toString(2)
+        for (i in 0..binaryValue.length-1) {
+            chars[35-i] = binaryValue[binaryValue.length - i - 1]
+        }
+
+        for(i in 0..35) {
+            if(mask[i] == '1' || mask[i] == '0') {
+                chars[i] = mask[i]
+            }
+        }
+        return chars.concatToString().toLong(2)
+    }
+
+    fun runProgram(): Long {
+        for(instruction in instructions) {
+            if(instruction.first == "mask" ) {
+                currentMask = instruction.second
+            } else {
+                val memLocation = memoryRegex.find(instruction.first)?.groupValues?.get(1)?.toInt()
+                val memoryValue = applyMask(instruction.second.toLong(), currentMask)
+                if (memLocation != null) {
+                    memory.put(memLocation, memoryValue)
+                }
+            }
+        }
+
+        return memory.values.reduce { acc, l -> acc + l }
+    }
+}
---
+++ src/test/kotlin/day14/DockingComputerTest.kt
@@ -0,0 +1,42 @@
+package day14
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@ExperimentalStdlibApi
+class DockingComputerTest {
+
+    @Test
+    internal fun testReadInstructions() {
+        val comp = DockingComputer(fileName = "src/test/resources/day14/test_input1.txt")
+        assertEquals(4, comp.instructions.size, "Incorrect number of instructions loaded")
+    }
+
+    @Test
+    internal fun testApplyMask() {
+        val comp = DockingComputer(fileName = "src/test/resources/day14/test_input1.txt")
+        val result = comp.applyMask(11L, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")
+        assertEquals(73L, result, "mask application incorrect")
+    }
+
+    @Test
+    internal fun testApplyMask2() {
+        val comp = DockingComputer(fileName = "src/test/resources/day14/test_input1.txt")
+        val result = comp.applyMask(101L, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")
+        assertEquals(101L, result, "mask application incorrect")
+    }
+
+    @Test
+    internal fun testApplyMask3() {
+        val comp = DockingComputer(fileName = "src/test/resources/day14/test_input1.txt")
+        val result = comp.applyMask(0L, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")
+        assertEquals(64L, result, "mask application incorrect")
+    }
+
+    @Test
+    internal fun testRunProgram() {
+        val comp = DockingComputer(fileName = "src/test/resources/day14/test_input1.txt")
+        assertEquals(165L, comp.runProgram(), "Program result not as expected" )
+    }
+
+}
